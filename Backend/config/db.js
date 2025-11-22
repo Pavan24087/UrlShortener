609a9587663f6c4f61e00aee596d2e8fd5941670
Pavan24087 <pavanbathini86@gmail.com>
2025-11-22 23:49:17 +0530

    added cors cofiguration

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -38,7 +38,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (err) {
     console.error("Error connecting MongoDB:", err);
